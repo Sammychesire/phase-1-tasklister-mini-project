dccea704f4247a74375320d65290046669f8df4b
bob <sammy.chesire@student.moringaschool.com>
2022-08-23 17:25:45 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector('#create-task-form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    handleToDo (form.querySelector('#new-task-description').value)
+    form.reset()
+  
+  })
+  
 });
+
+ function handleToDo(todo){
+    let p = document.createElement('p');
+  p.textContent = todo
+   let button = document.createElement('button')
+    button.textContent = ('x')
+    p.appendChild(button)
+   button.addEventListener('click', (e) => e.target.parentNode.remove())
+    document.querySelector('#tasks').appendChild(p)
+  }
